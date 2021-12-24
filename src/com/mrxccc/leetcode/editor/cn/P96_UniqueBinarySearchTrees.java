76ca4aae6b337f880442c67ba1aca234e76a30aa
caijc <caijc@133.cn>
2021-12-24 14:25:24 +0800

    增加已刷题目

--- src/com/mrxccc/leetcode/editor/cn/P96_UniqueBinarySearchTrees.java
+++ src/com/mrxccc/leetcode/editor/cn/P96_UniqueBinarySearchTrees.java
@@ -41,18 +41,27 @@ public class P96_UniqueBinarySearchTrees{
 
     //leetcode submit region begin(Prohibit modification and deletion)
     class Solution {
+        int[][] memo;
         public int numTrees(int n) {
+            // 备忘录的值初始化为 0
+            memo = new int[n + 1][n + 1];
             return count(1,n);
         }
 
         private int count(int lo, int hi) {
             if (lo > hi) return 1;
+            // 查备忘录
+            if (memo[lo][hi] != 0) {
+                return memo[lo][hi];
+            }
             int res = 0;
             for (int i = lo; i <= hi; i++){
                 int left = count(lo,i-1);
                 int right = count(i + 1, hi);
                 res += left*right;
             }
+            // 将结果存入备忘录
+            memo[lo][hi] = res;
             return res;
         }
     }
